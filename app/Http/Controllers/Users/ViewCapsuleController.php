8a13de1b03e4de2d761ac55b30d15d0a8ea9224d
MarounnGhayaa <marounghaya@icloud.com>
2025-07-17 20:53:53 +0300

    Added view capsule controller

--- app/Http/Controllers/Users/ViewCapsuleController.php
+++ app/Http/Controllers/Users/ViewCapsuleController.php
@@ -5,9 +5,20 @@ namespace App\Http\Controllers\Users;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Capsule;
 class ViewCapsuleController extends Controller
 {
     public function printView() {
         return ("Hello View!");
+    }
+
+    public function viewCapsule($id) {
+        $capsule = Capsule::find($id);
+
+        $response = [];
+        $response["status"] = "success";
+        $response["payload"] = $capsule;
+
+        return json_encode($response, 200);   
     }
 }
