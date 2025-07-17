3b8dba74e66b6df9f2d6354598b6eff0347878a1
MarounnGhayaa <marounghaya@icloud.com>
2025-07-17 21:12:50 +0300

    Added profile controller

--- app/Http/Controllers/Users/ProfileController.php
+++ app/Http/Controllers/Users/ProfileController.php
@@ -5,9 +5,20 @@ namespace App\Http\Controllers\Users;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\User;
 class ProfileController extends Controller
 {
     public function printPro() {
         return ("Hello Profile!");
+    }
+
+    public function getUserInfo($id) {
+        $user = User::find($id);
+
+        $response = [];
+        $response["status"] = "success";
+        $response["payload"] = $user;
+
+        return json_encode($response, 200);   
     }
 }
