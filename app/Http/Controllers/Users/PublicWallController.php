f5ecd4b9e230568bc789a90db0eccd16fd938316
MarounnGhayaa <marounghaya@icloud.com>
2025-07-17 20:49:20 +0300

    Created public wall controller

--- app/Http/Controllers/Users/PublicWallController.php
+++ app/Http/Controllers/Users/PublicWallController.php
@@ -5,9 +5,32 @@ namespace App\Http\Controllers\Users;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Capsule;
+
 class PublicWallController extends Controller
 {
     public function printPub() {
         return ("Hello Wall!");
+    }
+
+
+    public function getPublicCapsules(){
+        $capsules = Capsule::where('privacy', 'public')->get();
+
+        $response = [];
+        $response["status"] = "success";
+        $response["payload"] = $capsules;
+
+        return json_encode($response, 200);
+    }
+
+    public function getSpecificCapsule($id){
+        $capsule = Capsule::find($id);
+
+        $response = [];
+        $response["status"] = "success";
+        $response["payload"] = $capsule;
+
+        return json_encode($response, 200);
     }
 }
