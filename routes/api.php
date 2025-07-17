03a277706fc0ccb2e5e81dcb7dee98460a88b9c0
MarounnGhayaa <marounghaya@icloud.com>
2025-07-17 21:51:10 +0300

    Optimized routes (versioning and grouping by user routes)

--- routes/api.php
+++ routes/api.php
@@ -10,24 +10,29 @@ use App\Http\Controllers\Users\PublicWallController;
 use App\Http\Controllers\Users\RegisterController;
 use App\Http\Controllers\Users\ViewCapsuleController;
 
-Route::get('/printCapsule', [CreateCapsuleController::class, 'printCap']);
+Route::group(["prefix" =>"v0.1"], function(){
+    Route::group(["prefix" => "user"], function(){
 
-Route::get('/printDashboard', [DashboardController::class, 'printDash']);
-Route::get('/getUserCapsule/{id}', [DashboardController::class, 'getUserCapsules']);
-Route::get('/getTotalCapsulesNb/{id}', [DashboardController::class, 'getTotalCapsulesNb']);
-Route::get('/getPastCapsulesNb/{id}', [DashboardController::class, 'getPastCapsulesNb']);
-Route::get('/getUpcomingCapsulesNb/{id}', [DashboardController::class, 'getUpcomingCapsulesNb']);
+        Route::get('/printCapsule', [CreateCapsuleController::class, 'printCap']);
 
-Route::get('/printLogin', [LoginController::class, 'printLog']);
+        Route::get('/printDashboard', [DashboardController::class, 'printDash']);
+        Route::get('/getUserCapsule/{id}', [DashboardController::class, 'getUserCapsules']);
+        Route::get('/getTotalCapsulesNb/{id}', [DashboardController::class, 'getTotalCapsulesNb']);
+        Route::get('/getPastCapsulesNb/{id}', [DashboardController::class, 'getPastCapsulesNb']);
+        Route::get('/getUpcomingCapsulesNb/{id}', [DashboardController::class, 'getUpcomingCapsulesNb']);
 
-Route::get('/printProfile', [ProfileController::class, 'printPro']);
-Route::get('/getInfo/{id}', [ProfileController::class, 'getUserInfo']);
+        Route::get('/printLogin', [LoginController::class, 'printLog']);
 
-Route::get('/printPublicWall', [PublicWallController::class, 'printPub']);
-Route::get('/publicCapsules', [PublicWallController::class, 'getPublicCapsules']);
-Route::get('/getCapsule/{id}', [PublicWallController::class, 'getSpecificCapsule']);
+        Route::get('/printProfile', [ProfileController::class, 'printPro']);
+        Route::get('/getInfo/{id}', [ProfileController::class, 'getUserInfo']);
 
-Route::get('/printRegister', [RegisterController::class, 'printReg']);
+        Route::get('/printPublicWall', [PublicWallController::class, 'printPub']);
+        Route::get('/publicCapsules', [PublicWallController::class, 'getPublicCapsules']);
+        Route::get('/getCapsule/{id}', [PublicWallController::class, 'getSpecificCapsule']);
 
-Route::get('/printCapsule', [ViewCapsuleController::class, 'printView']);
-Route::get('/viewCapsule/{id}', [ViewCapsuleController::class, 'viewCapsule']);
+        Route::get('/printRegister', [RegisterController::class, 'printReg']);
+
+        Route::get('/printCapsule', [ViewCapsuleController::class, 'printView']);
+        Route::get('/viewCapsule/{id}', [ViewCapsuleController::class, 'viewCapsule']);
+    });
+});
