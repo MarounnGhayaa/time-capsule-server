3b646d42e3239c53a60863cd7f865129ee5c76d5
MarounnGhayaa <marounghaya@icloud.com>
2025-07-17 21:19:00 +0300

    Added dashboard controller with some capsules functions

--- app/Http/Controllers/Users/DashboardController.php
+++ app/Http/Controllers/Users/DashboardController.php
@@ -5,9 +5,53 @@ namespace App\Http\Controllers\Users;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Capsule;
 class DashboardController extends Controller
 {
     public function printDash() {
         return ("Hello Dashboard!");
     }
+
+    public function getUserCapsules($id) {
+        $capsules = Capsule::where('user_id', $id)->get();
+
+        $response = [];
+        $response["status"] = "success";
+        $response["payload"] = $capsules;
+        return json_encode($response, 200);
+    }
+
+    public function getTotalCapsulesNb($id){
+        $capsulesCount = Capsule::where('user_id', $id)->count();
+        $response = [];
+        $response["status"] = "success";
+        $response["payload"] = $capsulesCount;
+
+        return json_encode($response, 200);
+    }
+
+    public function getPastCapsulesNb($id) {
+        $pastCapsules = Capsule::where('user_id', $id)
+                        ->where('reveal_at', '<', now())
+                        ->count();
+
+        $response = [];
+        $response["status"] = "success";
+        $response["payload"] = $pastCapsules;
+
+        return json_encode($response, 200);
+    }
+
+        public function getUpcomingCapsulesNb($id) {
+        $upcomingCapsules = Capsule::where('user_id', $id)
+                        ->where('reveal_at', '>', now())
+                        ->count();
+
+        $response = [];
+        $response["status"] = "success";
+        $response["payload"] = $upcomingCapsules;
+
+        return json_encode($response, 200);
+    }
+
 }
