058c0d076758d1697da1fbf15d1da8cf27282132
MarounnGhayaa <marounghaya@icloud.com>
2025-07-18 13:31:24 +0300

    Added creating capsule method in its controller (CreateCapsuleController)

--- app/Http/Controllers/Users/CreateCapsuleController.php
+++ app/Http/Controllers/Users/CreateCapsuleController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-class CreateCapsuleController extends Controller
-{
+use App\Models\Capsule;
+use App\Services\CapsuleService;
+use App\Traits\ResponseTrait;
+class CreateCapsuleController extends Controller {
+    use ResponseTrait;
     public function printCap() {
         return ("Hello Capsule!");
+    }
+
+    public function addCapsule(Request $request) {
+        $capsule = new Capsule();
+        $data = $request->all();
+        $capsule = CapsuleService::createCapsule($data, $capsule);
+
+        return $this->responseJSON($capsule);
     }
 }
